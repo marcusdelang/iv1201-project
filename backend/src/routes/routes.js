555df6f089d87fe34adf10749146ddf12a7565d0
daniel-duner <daniel_duner@hotmail.com>
2020-02-14 14:05:04 +0100

    init branch

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,9 +2,13 @@
 const user = require('./user')
 const auth = require('./auth')
 const competence = require('./competence')
+const competences = require('./competences')
+const application = require('./application')
 
 module.exports = {
   user,
   auth,
   competence
+  competences,
+  application
 }
