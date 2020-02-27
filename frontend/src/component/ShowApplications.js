9a611b64c05bad1837becf8cd385762492a22140
daniel-duner <daniel_duner@hotmail.com>
2020-02-27 14:24:07 +0100

    fix

--- frontend/src/component/ShowApplications.js
+++ frontend/src/component/ShowApplications.js
@@ -46,6 +46,7 @@ class ShowApplication extends React.Component {
  }
 
   render() {
+    console.log(!(JSON.parse(this.props.appState.role) === 2)
     if (!this.props.appState.auth || !(JSON.parse(this.props.appState.role) === 2)) {
       return <Redirect to="/home" />;
     }
